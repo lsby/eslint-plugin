5d3595097cd7f2bd4f70219afdb6e1156959fd19
hbybyyang <2451759073@qq.com>
2024-12-06 07:45:30 +0800

    导出

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ module.exports = {
   rules: {
     'no-broken-link': require('./lib/rules/no-broken-link'),
     'prefer-let': require('./lib/rules/prefer-let'),
+    'no-negation': require('./lib/rules/no-negation'),
   },
 }
