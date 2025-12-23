60797b625a9168b39d736a6a7785c922352958b2
lsby <2451759073@qq.com>
2025-12-23 17:04:30 +0800

    整理

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ module.exports = {
     'no-broken-link': require('./lib/rules/no-broken-link'),
     'prefer-let': require('./lib/rules/prefer-let'),
     'no-negation': require('./lib/rules/no-negation'),
-    'no-null': require('./lib/rules/no-null'),
-    'require-has-check': require('./lib/rules/require-has-check'),
     'no-definite-assignment-assertion': require('./lib/rules/no-definite-assignment-assertion'),
     'no-else': require('./lib/rules/no-else'),
   },
